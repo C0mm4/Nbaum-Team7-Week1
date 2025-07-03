eeaed8f1f6c75a37a14e54e443482b1045c0af3d
muntory <38274669+muntory@users.noreply.github.com>
2025-07-03 12:01:04 +0900

    update result view

--- Assets/Script/ResultView.cs
+++ Assets/Script/ResultView.cs
@@ -6,25 +6,26 @@ using UnityEngine.UI;
 
 public class ResultView : MonoBehaviour
 {
-    [SerializeField]
-    Text BestClearTimeText;
     List<Text> dataTexts = new List<Text>();
     public void Init()
     {
-        Text[] texts = GetComponentsInChildren<Text>();
-        foreach (Text text in texts)
+        if (dataTexts.Count == 0)
         {
-            if (text.name.StartsWith("Data"))
+            Text[] texts = GetComponentsInChildren<Text>();
+            foreach (Text text in texts)
             {
-                dataTexts.Add(text);
+                if (text.name.StartsWith("Data"))
+                {
+                    dataTexts.Add(text);
+                }
             }
         }
 
         Debug.Log($"matchCombo : {GameData.matchCombo}, missCombo : {GameData.missCombo}");
 
-        dataTexts[0].text = GameData.maxCombo.ToString();
-        dataTexts[1].text = GameData.flipCount.ToString();
-        dataTexts[2].text = GameData.leftT.ToString("N2");
+        dataTexts[0].text = $"최대 콤보 횟수 : {GameData.maxCombo}";
+        dataTexts[1].text = $"카드 뒤집은 횟수 : {GameData.flipCount}";
+        dataTexts[2].text = $"이번 판 클리어 타임 : " + GameData.leftT.ToString("N2");
 
         string level;
         switch (GameManager.stageLevel)
@@ -46,21 +47,15 @@ public class ResultView : MonoBehaviour
                 break;
         }
 
-        BestClearTimeText.text = $"베스트 클리어 타임({level}) :";
-
-        Debug.Log("Check High score");
-        Debug.Log($"Time{GameManager.stageLevel}");
+        float bestClearTime = 0f;
         if (PlayerPrefs.HasKey($"Time{GameManager.stageLevel}"))
         {
-            float bestClearTime = PlayerPrefs.GetFloat($"Time{GameManager.stageLevel}");
-            dataTexts[3].text = bestClearTime.ToString("N2");
-        }
-        else
-        {
-            Debug.Log("no high score");
-            dataTexts[3].text = "00.00";
+            bestClearTime = PlayerPrefs.GetFloat($"Time{GameManager.stageLevel}");
+            
         }
 
-        dataTexts[4].text = ((float)InGameManager.Instance.Card_size / (float)GameData.flipCount).ToString("N2");
+        dataTexts[3].text = $"베스트 클리어 타임({level}) : " + bestClearTime.ToString("N2");
+        float success_ratio = ((float)(InGameManager.Instance.Card_size / 2) / (float)GameData.flipCount);
+        dataTexts[4].text = $"카드 매칭 성공률 : " + success_ratio.ToString("N2");
     }
 }
