a787c9948f681cae6b4b93ea17d7682324d19cf1
shunir <48938315+shunir@users.noreply.github.com>
2025-07-04 13:26:31 +0900

    tq

--- Assets/Script/SoundSliderUI.cs
+++ Assets/Script/SoundSliderUI.cs
@@ -13,6 +13,14 @@ public class SoundSliderUI : MonoBehaviour
     public Slider BGMSlider; // Reference to the UI Slider component
     public Slider EffectSlider;
 
+    public AudioMixer mixer;
+    public Slider masterslider;
+    public Slider bgmslider;
+    public Slider effectslider;
+
+
+
+
     public void SetBGM(AudioClip audioClip)
     {
         bgm.clip = audioClip;
@@ -21,16 +29,42 @@ public class SoundSliderUI : MonoBehaviour
     public void SetMasterVolume()
     {
         audioMixer.SetFloat("Master", Mathf.Log10(MasterSlider.value) *20);
+        PlayerPrefs.SetFloat("MasterVolume", MasterSlider.value);
     }
 
     public void SetBGMVolume()
     {
         audioMixer.SetFloat("BGM", Mathf.Log10(BGMSlider.value) * 20);
+        PlayerPrefs.SetFloat("BGMVolume", BGMSlider.value);
     }
 
     public void SetEffectVolume()
     {
         audioMixer.SetFloat("Effect", Mathf.Log10(EffectSlider.value) * 20);
+        PlayerPrefs.SetFloat("EffectVolume", EffectSlider.value);
     }
+
+    public void OnEnable()
+    {
+        masterslider.value = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
+        bgmslider.value = PlayerPrefs.GetFloat("BGMVolume", 0.5f);
+        effectslider.value = PlayerPrefs.GetFloat("EffectVolume", 0.5f);
+    }
+
+    public void OnDisable()
+    {
+        masterslider.onValueChanged.RemoveAllListeners();
+        bgmslider.onValueChanged.RemoveAllListeners();
+        effectslider.onValueChanged.RemoveAllListeners();
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+       mixer.SetFloat("Master", Mathf.Log10(sliderValue) * 20);
+       PlayerPrefs.SetFloat("MasterVolume", sliderValue);
+     
+    }
+
+ 
 }
 
