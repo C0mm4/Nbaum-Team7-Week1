f16a30cde54f9b9a63131f7d434b1e210ac53a48
muntory <38274669+muntory@users.noreply.github.com>
2025-07-04 12:11:08 +0900

    update configuration

--- Assets/Script/SoundSliderUI.cs
+++ Assets/Script/SoundSliderUI.cs
@@ -12,7 +12,7 @@ public class SoundSliderUI : MonoBehaviour
     public Slider MasterSlider; // Reference to the UI Slider component
     public Slider BGMSlider; // Reference to the UI Slider component
     public Slider EffectSlider;
-
+   
     public void SetBGM(AudioClip audioClip)
     {
         bgm.clip = audioClip;
@@ -20,16 +20,19 @@ public class SoundSliderUI : MonoBehaviour
 
     public void SetMasterVolume()
     {
+        PlayerPrefs.SetFloat("MasterVolumeValue", MasterSlider.value);
         audioMixer.SetFloat("Master", Mathf.Log10(MasterSlider.value) *20);
     }
 
     public void SetBGMVolume()
     {
+        PlayerPrefs.SetFloat("BGMVolumeValue", BGMSlider.value);
         audioMixer.SetFloat("BGM", Mathf.Log10(BGMSlider.value) * 20);
     }
 
     public void SetEffectVolume()
     {
+        PlayerPrefs.SetFloat("EffectVolumeValue", EffectSlider.value);
         audioMixer.SetFloat("Effect", Mathf.Log10(EffectSlider.value) * 20);
     }
 }
